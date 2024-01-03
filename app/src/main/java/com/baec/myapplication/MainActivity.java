5f9e0dbcc53611d6fe7c8cc53123e7feaf901ac9
BryanEnriquez1 <bryan.enriquez487@ist17dejulio.edu.ec>
2024-01-03 10:55:42 -0500

    CREACION DE PROYECTO SWITCH

---
+++ app/src/main/java/com/baec/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.baec.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView texto = findViewById(R.id.tvtexto);
+        Switch swtCambia = findViewById(R.id.swtCambio);
+        ConstraintLayout fondo = findViewById(R.id.clFondo);
+
+        swtCambia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int colorPlomo = Color.rgb(169, 169, 169);
+                if (view.getId()==R.id.swtCambio){
+                    if (swtCambia.isChecked()){
+                        texto.setText("ACTIVADO");
+                        fondo.setBackgroundColor(colorPlomo);
+                    }else{
+                        texto.setText("DESACTIVADO");
+                        fondo.setBackgroundColor(Color.WHITE);
+                    }
+                }
+            }
+        });
+    }
+}
